99fe0f1513218c195b8c2b2bb7e5a017e4652e67
kotendro <i.abramtsov@gmail.com>
2025-11-26 14:16:57 +0500

    feat: Дописал модель

--- server/database/models.py
+++ server/database/models.py
@@ -9,13 +9,42 @@ class Employee(db.Model):
     
     id_emp: Mapped[int] = mapped_column(primary_key=True)
     fullname: Mapped[str] = mapped_column(nullable=False)
-    birthday: Mapped[datetime] = mapped_column(db.DateTime(timezone=True))
+    birthday: Mapped[datetime] = mapped_column(db.DateTime(timezone=True), nullable=False)
+    id_cit: Mapped[int] = mapped_column(db.ForeignKey("citizenship.id_cit"), nullable=False)
+    id_edu: Mapped[int] = mapped_column(db.ForeignKey("education.id_edu"), nullable=False)
+    
     
 class Report(db.Model):
     __tablename__ = "report"
     
-    id_emp: Mapped
+    id_emp: Mapped[int] = mapped_column(db.ForeignKey("employee.id_emp"), primary_key=True)
+    num: Mapped[int] = mapped_column(primary_key=True)
+    date: Mapped[datetime] = mapped_column(db.DateTime(timezone=True), nullable=False)
+    id_div: Mapped[int] = mapped_column(db.ForeignKey("division.id_div"), nullable=False)
+    id_pos: Mapped[int] = mapped_column(db.ForeignKey("position.id_pos"), nullable=False)
+    old_salary: Mapped[float] = mapped_column(db.Numeric(12, 2), nullable=False)
     
-
+class Citizenship(db.Model):
+    __tablename__ = "citizenship"
     
-        
+    id_cit: Mapped[int] = mapped_column(primary_key=True)
+    name_cit: Mapped[str] = mapped_column(nullable=False, unique=True)
+    
+class Education(db.Model):
+    __tablename__ = "education"
+    
+    id_edu: Mapped[int] = mapped_column(primary_key=True)
+    name_edu: Mapped[str] = mapped_column(nullable=False, unique=True)
+    
+class Division(db.Model):
+    __tablename__ = "division"
+    
+    id_div: Mapped[int] = mapped_column(primary_key=True)
+    name_div: Mapped[str] = mapped_column(nullable=False, unique=True)
+    
+class Position(db.Model):
+    __tablename__ = "position"
+    
+    id_pos: Mapped[int] = mapped_column(primary_key=True)
+    name_pos: mapped_column[str] = mapped_column(nullable=False, unique=True)
+    salary: Mapped[float] = mapped_column(db.Numeric(12, 2), nullable=False)
